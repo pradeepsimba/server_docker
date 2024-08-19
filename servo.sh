ae1e40a1ec0e9b8c361009ed040adb624ab70aa3
pradeepsimba <96039278+pradeepsimba@users.noreply.github.com>
2024-08-19 11:39:33 +0530

    Update servo.sh

--- servo.sh
+++ servo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Build the Docker image once
-docker build -t myimage .
+docker build -t serimage .
 
 # Run the container 20 times in detached mode
 for i in {1..20}
 do
-   docker run -d --name mycontainer_$i myimage
+   docker run -d --name mycontainer_$i serimage
    # docker rm mycontainer_$i
 done
